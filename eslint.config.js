531aa7ce11131157629858310dd0ac723de6c3df
NotJarethGG <moragajareth@gmail.com>
2024-11-11 14:52:51 -0600

    config4

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      sourceType: "module",           // Define el tipo de módulo, si es necesario.
-      globals: globals.node,           // Habilita las variables globales de Node.js
-      ecmaVersion: "latest",           // Opcional: Define la versión de ECMAScript a utilizar
-    },
-  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { languageOptions: { sourceType: 'module', globals: { ...globals.node, ...globals.mocha } } },
   pluginJs.configs.recommended,
 ];
