77bc0345e75d3a130a338e269c92932f8783291b
NotJarethGG <moragajareth@gmail.com>
2024-11-11 14:47:17 -0600

    config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,15 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { sourceType: 'module', globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module",  // Cambiado a 'module' si usas `import`/`export`
+      globals: globals.node, // Activa el entorno de Node.js
+    },
+    env: { node: true }, // Activa el entorno para Node.js
+  },
   pluginJs.configs.recommended,
 ];
